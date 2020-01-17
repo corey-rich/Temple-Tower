20e6611b6a796d118614ccb9877c350630c35331
DK <davidkkimball@gmail.com>
2020-01-16 22:19:14 -0500

    Puma

--- Assets/Scripts/PumaController.cs
+++ Assets/Scripts/PumaController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyController : MonoBehaviour {
+public class PumaController : MonoBehaviour {
 
     public float lookRadius = 10f;
     public float speed;
