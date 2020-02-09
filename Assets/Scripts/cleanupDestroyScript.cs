d8ffb95c2e21e71c7c00c19cc9bfc6cbeb6c231c
DK <davidkkimball@gmail.com>
2020-02-09 12:43:45 -0500

    Destroy out of sight objects script

---
+++ Assets/Scripts/cleanupDestroyScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cleanupDestroyScript : MonoBehaviour
+{
+    private GameObject player;
+    public int manualDistance = 30;
+   // private Transform trans;
+    // Start is called before the first frame update
+    void Start()
+    {
+      player = GameObject.Find("MilesNewWorking");
+     // trans = player.GetComponent<Transform>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerDistance = Vector3.Distance(transform.position, player.transform.position);
+        if(playerDistance > manualDistance)
+        {
+            
+            DestroyGameObject();
+        }
+        //Debug.Log(playerDistance);
+    }
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
+    }
+}
