3e6e05471f53d120306a0f4d86194121e4e668e8
DK <davidkkimball@gmail.com>
2020-02-12 14:47:43 -0500

    addingTextures

--- Assets/Scripts/RotateTowards.cs
+++ Assets/Scripts/RotateTowards.cs
@@ -15,7 +15,7 @@ public class RotateTowards : MonoBehaviour
 
     void Update()
     {
-       /* // Determine which direction to rotate towards
+        // Determine which direction to rotate towards
         Vector3 targetDirection = target.position - transform.position;
 
         // The step size is equal to speed times frame time.
@@ -28,7 +28,7 @@ public class RotateTowards : MonoBehaviour
         Debug.DrawRay(transform.position, newDirection, Color.red);
 
         // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.LookRotation(newDirection);*/
+        transform.rotation = Quaternion.LookRotation(newDirection);
 
     }
 }
---
+++ Assets/levelChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class levelChanger : MonoBehaviour
+{
+    public string levelName;
+    public void GoToNextLevel()
+    {
+        //levelName = "'" + levelName + "'";
+        SceneManager.LoadScene(levelName);
+    }
+}
+
